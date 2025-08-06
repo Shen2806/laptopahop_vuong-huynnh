cfe708b50a0bf3bdf28efbc9c3d1c10fb5d70740
Shen2806 <dmvuong12a2@gmail.com>
2025-08-06 21:28:04 +0700

    video 34

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 // const express = require('express');
 import express from 'express';
 require('dotenv').config();
+import webRoutes from './routes/web';
 const app = express();
 
 // Set up EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+//configure web routes
+webRoutes(app);
+
 const PORT = process.env.PORT || 8080;
-app.get("/", (req, res) => {
-    res.render("home.ejs")
-});
+
 
 app.listen(PORT, () => {
     console.log(`My app is running on port : ${PORT}`);
