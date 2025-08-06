c660b5a6840933314b60b403ae23d918aee2331b
Shen2806 <dmvuong12a2@gmail.com>
2025-08-06 20:59:36 +0700

    video 31

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 // const express = require('express');
 import express from 'express';
-
+require('dotenv').config();
 const app = express();
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 app.get("/", (req, res) => {
-    res.send("Hello Minh Vuong");
+    res.send("Hello Minh Vuong - nodemon");
 
 });
 
