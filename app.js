aed37a8c280ab91c5f51f23ba194c746cbb0a406
Shen2806 <dmvuong12a2@gmail.com>
2025-08-06 10:56:16 +0700

    video 24

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+const express = require('express');
+
+const app = express();
+const PORT = 8080
+app.get("/", (req, res)=>{
+    res.send ("Hello Minh Vuong");
+
+});
+
+app.listen(PORT, () => {
+    console.log(`My app is running on port : ${PORT}`);    
+});
