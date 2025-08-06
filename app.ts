4287c623ec6095a3722455b53961d7e6516e8cce
Shen2806 <dmvuong12a2@gmail.com>
2025-08-06 11:11:57 +0700

    video 225

---
+++ app.ts
@@ -0,0 +1,13 @@
+// const express = require('express');
+import express from 'express';
+
+const app = express();
+const PORT = 8080
+app.get("/", (req, res) => {
+    res.send("Hello Minh Vuong");
+
+});
+
+app.listen(PORT, () => {
+    console.log(`My app is running on port : ${PORT}`);
+});
