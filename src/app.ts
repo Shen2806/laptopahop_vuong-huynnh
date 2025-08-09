5a626ece379a97cbcfcb6e011e676d1667142264
Shen2806 <dmvuong12a2@gmail.com>
2025-08-09 09:38:22 +0700

    video 45

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import express from 'express';
 require('dotenv').config();
 import webRoutes from './routes/web';
+import getConnection from './config/database';
 const app = express();
 
 // Set up EJS as the view engine
@@ -17,7 +18,7 @@ const PORT = process.env.PORT || 8080;
 webRoutes(app);
 
 
-
+getConnection()
 app.listen(PORT, () => {
     console.log(`My app is running on port : ${PORT}`);
 });
