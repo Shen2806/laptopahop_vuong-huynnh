a4e87bfa9cd6fda06a7d62cbd80d7257440c367f
Shen2806 <dmvuong12a2@gmail.com>
2025-08-22 16:18:53 +0700

    video 83

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -15,10 +15,11 @@ const getCreateUserPage = async (req: Request, res: Response) => {
 }
 const postCreateUser = async (req: Request, res: Response) => {
     const { fullName, username, phone, role, address } = req.body;
-
+    const file = req.file;
+    const avatar = file?.filename ?? '';
     // handle create user logic
-    // await handleCreateUser(fullName, email, address);
-    // return res.redirect("/");
+    await handleCreateUser(fullName, username, address, phone, avatar);
+    return res.redirect("/admin/user");
 }
 
 const postDeleteUser = async (req: Request, res: Response) => {
