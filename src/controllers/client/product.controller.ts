ddd496c0557385d7a49de705df125d208a47d6df
Shen2806 <dmvuong12a2@gmail.com>
2025-08-23 17:40:59 +0700

    video 95

--- src/controllers/client/product.controller.ts
+++ src/controllers/client/product.controller.ts
@@ -3,4 +3,6 @@ import { Request, Response } from "express";
 const getProductPage = async (req: Request, res: Response) => {
     return res.render("product/detail");
 }
+
+
 export { getProductPage };
